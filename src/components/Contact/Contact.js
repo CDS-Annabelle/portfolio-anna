fe179ba897dd15e3d1bea9d08313e55c465302e2
CDS <cdscontact@creation-de-site.eu>
2023-07-12 14:39:18 +0200

    add projects

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,38 @@
 import React from 'react'
+import './contact.css'
+
+import { useRef } from 'react'
 
 const Contact = () => {
-  return <div>Contact</div>
+  const form = useRef()
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+
+    e.target.reset()
+  }
+
+  return (
+    <section id="contact">
+      <h2>Contactez-moi</h2>
+
+      <div className="container contact__container">
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name="name" placeholder="Nom $ Prénom" required />
+          <input type="email" name="email" placeholder="Votre Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Votre Message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Envoyez un message
+          </button>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
