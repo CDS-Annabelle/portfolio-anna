4817171717177682bf8112a01ca7b2fd606260ea
CDS <cdscontact@creation-de-site.eu>
2023-07-20 00:02:28 +0200

    update about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import './about.css'
+import styles from './About.module.css'
 import anna from '../../assets/picture-anna.jpg'
 
 const About = () => {
   return (
     <section id="about">
-      <div className="parallax">
-        <div className="container about__container">
-          <div className="about__me">
-            <div className="about__me-image">
+      <div className={styles.parallax}>
+        <div className={styles.aboutContainer}>
+          <div className={styles.aboutMe}>
+            <div className={styles.aboutMeImage}>
               <img src={anna} alt="Annabelle Portfolio" />
             </div>
           </div>
 
-          <div className="about__content">
-            <div className="about__cards">
+          <div className={styles.aboutContent}>
+            <div className={styles.aboutCards}>
               <h2>A propos</h2>
-              <p className="textAbout">
+              <p className={styles.textAbout}>
                 En freelance, depuis 17 ans, je me suis spécialisée dans la
-                création de site, principalement ecommerce. Après avoir
-                travaillé sur plusieurs CMS, j'ai suivi deux formations ces deux
-                dernières années pour améliorer mes compétences en
-                développement, backend et frontend.
+                création de site, principalement ecommerce. <br />
+                Après avoir travaillé sur plusieurs CMS (Prestahashop et Joomla)
+                pour mes clients, j'ai une nette préférence désormais pour
+                Wordpress/ Woocommerce. <br />
+                J'ai depuis suivi en parallèle ces deux dernières années deux
+                formations pour améliorer mes compétences en développement
+                backend et frontend.
               </p>
             </div>
           </div>
