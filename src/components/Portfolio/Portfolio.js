077866f86c73f8039e539607ca834ef1c636610e
CDS <cdscontact@creation-de-site.eu>
2023-07-20 14:10:53 +0200

    refactoring

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,59 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import data from '../../assets/data.json'
 import clients from '../../assets/clients.json'
 import './portfolio.css'
 
-const tabs = [...document.querySelectorAll('.tab')]
-
-tabs.forEach((tab) => tab.addEventListener('click', tabsAnimation))
-
-const tabContents = [...document.querySelectorAll('.tab-content')]
-
-function tabsAnimation(e) {
-  const indexToRemove = tabs.findIndex((tab) =>
-    tab.classList.contains('active-tab')
-  )
-
-  tabs[indexToRemove].setAttribute('aria-selected', 'false')
-  tabs[indexToRemove].setAttribute('tabIndex', '-1')
-  tabs[indexToRemove].classList.remove('active-tab')
-  tabContents[indexToRemove].classList.remove('active-tab-content')
-
-  const indexToShow = tabs.indexOf(e.target)
-
-  tabs[indexToShow].setAttribute('tabIndex', '0')
-  tabs[indexToShow].setAttribute('aria-selected', 'true')
-  tabs[indexToShow].classList.add('active-tab')
-  tabContents[indexToShow].classList.add('active-tab-content')
-}
-
-tabs.forEach((tab) => tab.addEventListener('keydown', arrowNavigation))
-
-let tabFocus = 0
-function arrowNavigation(e) {
-  if (e.keyCode === 39 || e.keyCode === 37) {
-    tabs[tabFocus].setAttribute('tabIndex', -1)
-
-    if (e.keyCode === 39) {
-      tabFocus++
-
-      if (tabFocus >= tabs.length) {
-        tabFocus = 0
-      }
-    } else if (e.keyCode === 37) {
-      tabFocus--
-
-      if (tabFocus < 0) {
-        tabFocus = tabs.length - 1
-      }
-    }
-
-    tabs[tabFocus].setAttribute('tabIndex', 0)
-    tabs[tabFocus].focus()
-  }
-}
-
 const Portfolio = () => {
+  const [activeTab, setActiveTab] = useState(0)
+
+  const handleTabClick = (index) => {
+    setActiveTab(index)
+  }
+
   return (
     <section id="portfolio">
       <h2 className="titlePortfolio">Portfolio</h2>
@@ -69,9 +25,10 @@ const Portfolio = () => {
             aria-controls="panel-1"
             id="tab-1"
             type="button"
-            aria-selected="true"
-            tabIndex="0"
-            className="tab active-tab"
+            aria-selected={activeTab === 0 ? 'true' : 'false'}
+            tabIndex={activeTab === 0 ? '0' : '-1'}
+            className={activeTab === 0 ? 'tab active-tab' : 'tab'}
+            onClick={() => handleTabClick(0)}
           >
             Projets Perso
           </button>
@@ -80,33 +37,26 @@ const Portfolio = () => {
             aria-controls="panel-2"
             id="tab-2"
             type="button"
-            aria-selected="false"
-            tabIndex="-1"
-            className="tab"
+            aria-selected={activeTab === 1 ? 'true' : 'false'}
+            tabIndex={activeTab === 1 ? '0' : '-1'}
+            className={activeTab === 1 ? 'tab active-tab' : 'tab'}
+            onClick={() => handleTabClick(1)}
           >
             Projets Clients
           </button>
-          <button
-            role="tab"
-            aria-controls="panel-3"
-            id="tab-3"
-            type="button"
-            aria-selected="false"
-            tabIndex="-1"
-            className="tab"
-          >
-            Wordpress
-          </button>
         </div>
+
         <div
           id="panel-1"
           role="tabpanel"
           tabIndex="0"
           aria-labelledby="tab-1"
-          className="tab-content active-tab-content"
+          className={
+            activeTab === 0 ? 'tab-content active-tab-content' : 'tab-content'
+          }
         >
           <div className="portfolio__container">
-            {data.map(({ id, image, title, github, demo, text }) => {
+            {data.map(({ id, image, title, github, text }) => {
               return (
                 <article key={id} className="portfolio__item">
                   <div className="card1">
@@ -115,15 +65,13 @@ const Portfolio = () => {
                     </div>
                     <h3>{title}</h3>
                     <div className="portfolio__item-cta">
-                      <a href={github} className="btn" target="_blank">
-                        Github
-                      </a>
                       <a
-                        href={demo}
-                        className="btn btn-primary"
+                        href={github}
+                        className="btn"
                         target="_blank"
+                        rel="noreferrer"
                       >
-                        Live Demo
+                        Github
                       </a>
                     </div>
                   </div>
@@ -141,7 +89,9 @@ const Portfolio = () => {
           role="tabpanel"
           tabIndex="0"
           aria-labelledby="tab-2"
-          className="tab-content"
+          className={
+            activeTab === 1 ? 'tab-content active-tab-content' : 'tab-content'
+          }
         >
           <div className="portfolio__container">
             {clients.map(({ id, image, title, demo, text }) => {
@@ -157,13 +107,14 @@ const Portfolio = () => {
                         href={demo}
                         className="btn btn-primary"
                         target="_blank"
+                        rel="noreferrer"
                       >
                         Live Demo
                       </a>
                     </div>
                   </div>
                   <div className="card2">
-                    <p>{text} </p>
+                    <p>{text}</p>
                   </div>
                 </article>
               )
